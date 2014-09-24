ebd2b335b58051fe08e0d3ff371534265575947a
Winnie <winnie@yetihq.com>
2014-09-24 16:55:48 -0700

    Updated follow serializer

--- rest_social/serializers.py
+++ rest_social/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
+from rest_framework.pagination import PaginationSerializer
 from rest_social.rest_social.models import Tag, Comment, Follow, Flag
 from rest_user.rest_user.serializers import UserSerializer
+from videos.models import User
 
 __author__ = 'baylee'
 
@@ -20,9 +22,20 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class FollowSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
+    user_following = serializers.SerializerMethodField('get_user_follow')
 
     class Meta:
         model = Follow
+
+    def get_user_follow(self, obj):
+        user = User.objects.get(pk=obj.object_id)
+        serializer = UserSerializer(user)
+        return serializer.data
+
+
+class PaginatedFollowSerializer(PaginationSerializer):
+    class Meta:
+        object_serializer_class = FollowSerializer
 
 
 class FlagSerializer(serializers.ModelSerializer):
