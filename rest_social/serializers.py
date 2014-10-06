efde53048e8c3510f0c8f8efdf917edffb5434db
Baylee Feore <baylee@yetihq.com>
2014-10-06 11:35:34 -0700

    Username is not required for social sign up request

--- rest_social/serializers.py
+++ rest_social/serializers.py
@@ -80,3 +80,4 @@ class FollowPaginationSerializer(PaginationSerializer):
 class SocialSignUpSerializer(LoginSerializer):
     class Meta(LoginSerializer.Meta):
         fields = ('email', 'username', 'client_id', 'client_secret')
+        read_only_fields = ('username',)
